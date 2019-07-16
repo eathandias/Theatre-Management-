545d7a65c9d1e795703b85d612ca20ad9893ce73
Bevan Lewis <51845347+MacePelican@users.noreply.github.com>
2019-07-16 18:18:27 +0400

    Update project.py

--- project.py
+++ project.py
@@ -3,5 +3,4 @@ mydb=pvp.connect(host="localhost",user="root",password="Eathan234")
 mycursor=mydb.cursor()
 mycursor.execute("CREATE DATABASE movies")
 
-#This code works, also im testing how collabrative correction
-#takes place
+#This code works, also im testing how collabrative correction takes place
