329f3666fe92bc5261e6a6cfbab81fbb6abaf9d8
Bevan Lewis <51845347+MacePelican@users.noreply.github.com>
2019-07-16 17:53:41 +0400

    Update project.py

--- project.py
+++ project.py
@@ -2,3 +2,6 @@ import mysql.connector as pvp
 mydb=pvp.connect(host="localhost",user="root",password="Eathan234")
 mycursor=mydb.cursor()
 mycursor.execute("CREATE DATABASE movies")
+
+#This code works, also im testing how collabrative correction
+#takes place
